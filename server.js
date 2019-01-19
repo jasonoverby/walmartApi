4fe9ec0dbcd32f54e20b7456f751474434d493a6
jasonoverby <disguisekit@gmail.com>
2019-01-18 16:38:44 -0800

    add server.js file

---
+++ server.js
@@ -0,0 +1,109 @@
+const hapi = require('hapi');
+const vision = require('vision');
+const inert = require('inert');
+const hapiReactViews = require('hapi-react-views');
+
+const path = require('path');
+
+const {
+  getMatchingProducts,
+} = require('./lib/getMatchingProducts');
+
+require('babel-core/register')({
+  presets: ['react', 'env'],
+});
+
+const main = async () => {
+  // server at http://localhost:8000
+  const server = hapi.server({
+    host: 'localhost',
+    port: 8000,
+    routes: {
+      // routes requests for static files through assets dir
+      files: {
+        relativeTo: path.join(__dirname, 'assets'),
+      },
+    },
+  });
+
+  // hapi plugins for views
+  await server.register([inert, vision]);
+
+  // template options
+  server.views({
+    engines: {
+      jsx: hapiReactViews,
+    },
+    relativeTo: __dirname,
+    path: 'components',
+    compileOptions: {
+      // preserves the data-react-id attribute to allow for client-side
+      // re-rendering
+      renderMethod: 'renderToString',
+      layoutPath: path.join(__dirname, 'components'),
+      layout: 'Html',
+    },
+  });
+
+  // webpack bundled js
+  server.route({
+    method: 'GET',
+    path: '/bundle.js',
+    // uses inert plugin for static file
+    handler: {
+      file: 'bundle.js',
+    },
+  });
+
+  // css file
+  server.route({
+    method: 'GET',
+    path: '/main.css',
+    // uses inert plugin for static file
+    handler: {
+      file: 'main.css',
+    },
+  });
+
+  // root path
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: async (request, h) => {
+      const context = {
+        isResultsPage: false,
+      };
+
+      // sets string to be written to script in html component
+      context.state = `window.state = ${JSON.stringify(context)};`;
+
+      return h.view('App', context);
+    },
+  });
+
+  // route to handle request to any category
+  server.route({
+    method: 'GET',
+    path: '/{category}',
+    handler: async (request, h) => {
+      // guard against injection attacks
+      const category = encodeURIComponent(request.params.category);
+      const matchingProducts = await getMatchingProducts(category);
+      const context = {
+        query: category,
+        products: matchingProducts,
+        isResultsPage: true,
+      };
+
+      // sets string to be written to script in html component
+      context.state = `window.state = ${JSON.stringify(context)};`;
+
+      return h.view('App', context);
+    },
+  });
+
+  await server.start();
+  console.log(`Server is listening at ${server.info.uri}`);
+};
+
+main();
