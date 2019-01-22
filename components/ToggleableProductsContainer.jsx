cabca8620842b400008f64c015a2532c3bdd595e
jasonoverby <disguisekit@gmail.com>
2019-01-22 06:28:29 -0800

    remove extra newline

--- components/ToggleableProductsContainer.jsx
+++ components/ToggleableProductsContainer.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import ProductsContainer from './ProductsContainer';
 import Product from './Product';
 
-
 class ToggleableProductsContainer extends React.Component {
   state = {
     viewProduct: false,
